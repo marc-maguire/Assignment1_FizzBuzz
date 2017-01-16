dcbafdf8ff376c74f6f538f9f133718b5f5927df
Marc Maguire <maguire.marc@gmail.com>
2017-01-16 14:29:41 +0800

    Initial Commit

---
+++ FizzBuzzC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzzC
+//
+//  Created by Marc Maguire on 2017-01-16.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
